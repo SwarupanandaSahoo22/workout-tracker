710ab98a4f7290c769713f0647c131051af68096
SwarupanandaSahoo22 <swarupanandasahoo22@gmail.com>
2026-03-20 08:50:12 +0530

    Removed extra project

--- car.py
+++
@@ -1,6 +0,0 @@
-class Car:
-    def __init__(self, model, year, colour, for_sale):
-        self.model = model
-        self.year = year
-        self.colour = colour
-        self.for_sale = for_sale
